d61c87955e45bacc6a352e4f953f10b359dc7c51
Dhanish2603 <dhanishpatel.200410107076@svitvasad.ac.in>
2023-08-30 13:46:57 +0530

    string easy completed

---
+++ Practice/Strings/Answers/binarysegment.java
@@ -0,0 +1,7 @@
+package Practice.Strings.Answers;
+
+public class binarysegment {
+    public boolean checkOnesSegment(String s) {
+        return !s.contains("01"); // return false if string contains "01"
+        }
+}
---
+++ Practice/Strings/Answers/maxrepeat.java
@@ -0,0 +1,15 @@
+package Practice.Strings.Answers;
+
+public class maxrepeat {
+    public int maxRepeating(String s, String w) {
+        if(w.length()>s.length()) return 0;
+        int ans=0;
+        StringBuilder sb=new StringBuilder("");
+        while(sb.length()<=s.length()){
+            sb.append(w);
+            if(s.contains(sb)) ans++;
+            else break;
+        }
+        return ans;
+    }
+}
