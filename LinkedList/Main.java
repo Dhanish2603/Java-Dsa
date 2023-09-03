96f20efbb200b09bcd18ddb989d0417180e5ce5a
Dhanish2603 <dhanishpatel.200410107076@svitvasad.ac.in>
2023-09-03 09:09:12 +0530

    remove linkedlist bugs

--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -1,21 +1,21 @@
-package LinkedList;
+// package LinkedList;
 
-public class Main {
-    public static void main(String[] args) {
+// public class Main {
+//     public static void main(String[] args) {
 
-        // Ll l = new Ll();
-        // l.insertFirst(12);
-        // l.insertFirst(45);
-        // l.insertFirst(23);
-        // l.insert(143, 1);
-        // l.insertLast(76);
-        // l.display();
+//         // Ll l = new Ll();
+//         // l.insertFirst(12);
+//         // l.insertFirst(45);
+//         // l.insertFirst(23);
+//         // l.insert(143, 1);
+//         // l.insertLast(76);
+//         // l.display();
 
-        DLL dl = new DLL();
-        dl.insertFirst(10);
-        dl.insertFirst(20);
-        dl.insertLast(30);
-        dl.insertLast(40);
-        dl.display();
-    }
-}
+//         DLL dl = new DLL();
+//         dl.insertFirst(10);
+//         dl.insertFirst(20);
+//         dl.insertLast(30);
+//         dl.insertLast(40);
+//         dl.display();
+//     }
+// }
